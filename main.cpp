5456d5a4f765844aa23defc2fd24b01204dd35b5
Khtonya <Apt.Is202@gmail.com>
2026-03-09 21:56:39 +0300

    Add class Scene, add test imput

--- main.cpp
+++ main.cpp
@@ -2,42 +2,69 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-const int WIDTH = 800, HEIGHT = 600;
+const int WIDTH = 1920, HEIGHT = 1080;
 
-int main(int argc, char *argv[]) {
+class Scene{
+private:
+  SDL_Event event;
   SDL_Window *window;
   SDL_Renderer *renderer;
-  if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-    printf("SDL_Init failed: %s\n", SDL_GetError());
-    return 1;
-  }
+  bool running = false;
+  int ScreenWith = 0;
+  int ScreenHeight = 0;
+  int i = 0;
+  int Color[3][3] = {
+    {255,0,0},
+    {0,255,0},
+    {0,0,255}
+  };
+public:
+  Scene(){
+    window = SDL_CreateWindow("0",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,WIDTH, HEIGHT,SDL_WINDOW_FULLSCREEN);
+    renderer = SDL_CreateRenderer(window, -1, 0);
 
-  window = SDL_CreateWindow("Hello, World!",
-                                    SDL_WINDOWPOS_UNDEFINED,
-                                    SDL_WINDOWPOS_UNDEFINED,
-                                    WIDTH, HEIGHT,
-                                    SDL_WINDOW_ALLOW_HIGHDPI);
-  if(window == NULL) {
-    printf("Could not create window: %s\n", SDL_GetError());
-    return 1;
+    running = true;
   }
-  
-  renderer = SDL_CreateRenderer(window, -1, 0);
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-  SDL_RenderClear(renderer);
-
-  SDL_RenderPresent(renderer);
-  
-  SDL_Event event;
-  while(1) {
-    if(SDL_PollEvent(&event)) {
-      if(event.type == SDL_QUIT) {
+  void run(){
+      while (running) {
+          imput();
+          update();
+          rendor();
+      }
+   }
+  void imput(){
+    while(SDL_PollEvent(&event)){
+      switch (event.type)
+      {
+      case SDL_QUIT:
+        running = false;
+        break;
+      case SDL_MOUSEBUTTONDOWN:
+        if(event.button.button == SDL_BUTTON_LEFT){
+          if(i == 2){
+            i = 0;}
+          else{ 
+            i+=1;}
+        }
+        break;
+      default:  
         break;
       }
+
     }
   }
+  void update(){}
+  void rendor(){
+    SDL_SetRenderDrawColor(renderer, Color[i][0], Color[i][1], Color[i][2], 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+  }
+};
+
+int main() {
+  
+  Scene scene;
+  scene.run();
   return 0;
 }
